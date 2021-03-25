64d9bd2b6fa9e87e42acbc51d927136356293b18
Pedro Lima <pedrogglima@gmail.com>
2021-03-25 14:24:13 -0300

    add renamed record classes for specing on db schema

--- spec/setup/db/schema.rb
+++ spec/setup/db/schema.rb
@@ -7,7 +7,32 @@ ActiveRecord::Schema.define do
     t.timestamps
   end
 
+  create_table :cups, force: true do |t|
+    t.integer 'year', null: false
+
+    t.timestamps
+  end
+
   create_table :matches, force: true do |t|
+    t.bigint 'championship_id', null: false
+    t.bigint 'team_id', null: false
+    t.bigint 'opponent_id', null: false
+    t.bigint 'round_id', null: false
+    t.integer 'id_match', null: false
+    t.string 'updates'
+    t.string 'place'
+    t.datetime 'date'
+    t.string 'start_at'
+    t.bigint 'round_id', null: false
+    t.integer 'team_score'
+    t.integer 'opponent_score'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+
+    t.timestamps
+  end
+
+  create_table :games, force: true do |t|
     t.bigint 'championship_id', null: false
     t.bigint 'team_id', null: false
     t.bigint 'opponent_id', null: false
@@ -47,7 +72,35 @@ ActiveRecord::Schema.define do
     t.timestamps
   end
 
+  create_table :table_rows, force: true do |t|
+    t.bigint 'championship_id', null: false
+    t.bigint 'team_id', null: false
+    t.bigint 'next_opponent_id'
+    t.integer 'posicao', null: false
+    t.integer 'pontos'
+    t.integer 'jogos'
+    t.integer 'vitorias'
+    t.integer 'empates'
+    t.integer 'derrotas'
+    t.integer 'gols_pro'
+    t.integer 'gols_contra'
+    t.integer 'saldo_de_gols'
+    t.integer 'cartoes_amarelos'
+    t.integer 'cartoes_vermelhos'
+    t.integer 'aproveitamento'
+    t.string 'recentes'
+
+    t.timestamps
+  end
+
   create_table :rounds, force: true do |t|
+    t.bigint 'championship_id', null: false
+    t.integer 'number', null: false
+
+    t.timestamps
+  end
+
+  create_table :series, force: true do |t|
     t.bigint 'championship_id', null: false
     t.integer 'number', null: false
 
@@ -58,6 +111,14 @@ ActiveRecord::Schema.define do
     t.string 'name', null: false
     t.string 'state'
     t.string 'avatar_url'
+
+    t.timestamps
+  end
+
+  create_table :clubs, force: true do |t|
+    t.string 'name', null: false
+    t.string 'state'
+    t.string 'club_flag_url'
 
     t.timestamps
   end
