01f5d677d46ec798ceb35651f15ae9577ce1ae63
Pedro Lima <pedrogglima@gmail.com>
2021-03-25 14:22:19 -0300

    add renamed record classes for specing

--- spec/support/initialize_record_classes.rb
+++ spec/support/initialize_record_classes.rb
@@ -13,3 +13,17 @@ record_classes.each do |record_class|
   # set ActiveRecord::Base as parent class
   Object.const_set(record_class, Class.new(ActiveRecord::Base))
 end
+
+renamed_record_class = %w[
+  Cup
+  Game
+  TableRow
+  Series
+  Club
+]
+
+renamed_record_class.each do |record_class|
+  # define record classes to specs
+  # set ActiveRecord::Base as parent class
+  Object.const_set(record_class, Class.new(ActiveRecord::Base))
+end
