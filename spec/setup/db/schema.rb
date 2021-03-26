f7b62abee17abd05881f64228cb3316a96dc572c
Pedro Lima <pedrogglima@gmail.com>
2021-03-26 18:32:52 -0300

    add null false to attrs on schema

--- spec/setup/db/schema.rb
+++ spec/setup/db/schema.rb
@@ -31,12 +31,12 @@ ActiveRecord::Schema.define do
     t.bigint 'opponent_id', null: false
     t.bigint 'round_id', null: false
     t.integer 'id_match', null: false
-    t.string 'updates'
-    t.string 'place'
-    t.datetime 'date'
-    t.string 'start_at'
-    t.integer 'team_score'
-    t.integer 'opponent_score'
+    t.string 'updates', null: false
+    t.string 'place', null: false
+    t.datetime 'date', null: false
+    t.string 'start_at', null: false
+    t.integer 'team_score', null: false
+    t.integer 'opponent_score', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
 
@@ -49,11 +49,11 @@ ActiveRecord::Schema.define do
     t.bigint 'team_opponent_id', null: false
     t.bigint 'match_round_id', null: false
     t.integer 'identifier', null: false
-    t.string 'place'
-    t.datetime 'date'
-    t.string 'start_at'
-    t.integer 'team_score'
-    t.integer 'opponent_score'
+    t.string 'place', null: false
+    t.datetime 'date', null: false
+    t.string 'start_at', null: false
+    t.integer 'team_score', null: false
+    t.integer 'opponent_score', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
 
@@ -67,12 +67,12 @@ ActiveRecord::Schema.define do
     t.bigint 'opponent', null: false
     t.bigint 'round', null: false
     t.integer 'id_match', null: false
-    t.string 'updates'
-    t.string 'place'
-    t.datetime 'date'
-    t.string 'start_at'
-    t.integer 'team_score'
-    t.integer 'opponent_score'
+    t.string 'updates', null: false
+    t.string 'place', null: false
+    t.datetime 'date', null: false
+    t.string 'start_at', null: false
+    t.integer 'team_score', null: false
+    t.integer 'opponent_score', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
 
@@ -89,18 +89,18 @@ ActiveRecord::Schema.define do
     t.bigint 'team_id', null: false
     t.bigint 'next_opponent_id'
     t.integer 'posicao', null: false
-    t.integer 'pontos'
-    t.integer 'jogos'
-    t.integer 'vitorias'
-    t.integer 'empates'
-    t.integer 'derrotas'
-    t.integer 'gols_pro'
-    t.integer 'gols_contra'
-    t.integer 'saldo_de_gols'
-    t.integer 'cartoes_amarelos'
-    t.integer 'cartoes_vermelhos'
-    t.integer 'aproveitamento'
-    t.string 'recentes'
+    t.integer 'pontos', null: false
+    t.integer 'jogos', null: false
+    t.integer 'vitorias', null: false
+    t.integer 'empates', null: false
+    t.integer 'derrotas', null: false
+    t.integer 'gols_pro', null: false
+    t.integer 'gols_contra', null: false
+    t.integer 'saldo_de_gols', null: false
+    t.integer 'cartoes_amarelos', null: false
+    t.integer 'cartoes_vermelhos', null: false
+    t.integer 'aproveitamento', null: false
+    t.string 'recentes', null: false
 
     t.timestamps
   end
@@ -110,13 +110,13 @@ ActiveRecord::Schema.define do
     t.bigint 'rank_team_id', null: false
     t.bigint 'next_team_opponent_id'
     t.integer 'rank', null: false
-    t.integer 'pontos'
-    t.integer 'jogos'
-    t.integer 'vitorias'
-    t.integer 'empates'
-    t.integer 'derrotas'
-    t.integer 'aproveitamento'
-    t.string 'recentes'
+    t.integer 'pontos', null: false
+    t.integer 'jogos', null: false
+    t.integer 'vitorias', null: false
+    t.integer 'empates', null: false
+    t.integer 'derrotas', null: false
+    t.integer 'aproveitamento', null: false
+    t.string 'recentes', null: false
 
     t.timestamps
   end
@@ -127,18 +127,18 @@ ActiveRecord::Schema.define do
     t.bigint 'team', null: false
     t.bigint 'next_opponent'
     t.integer 'posicao', null: false
-    t.integer 'pontos'
-    t.integer 'jogos'
-    t.integer 'vitorias'
-    t.integer 'empates'
-    t.integer 'derrotas'
-    t.integer 'gols_pro'
-    t.integer 'gols_contra'
-    t.integer 'saldo_de_gols'
-    t.integer 'cartoes_amarelos'
-    t.integer 'cartoes_vermelhos'
-    t.integer 'aproveitamento'
-    t.string 'recentes'
+    t.integer 'pontos', null: false
+    t.integer 'jogos', null: false
+    t.integer 'vitorias', null: false
+    t.integer 'empates', null: false
+    t.integer 'derrotas', null: false
+    t.integer 'gols_pro', null: false
+    t.integer 'gols_contra', null: false
+    t.integer 'saldo_de_gols', null: false
+    t.integer 'cartoes_amarelos', null: false
+    t.integer 'cartoes_vermelhos', null: false
+    t.integer 'aproveitamento', null: false
+    t.string 'recentes', null: false
 
     t.timestamps
   end
@@ -177,8 +177,8 @@ ActiveRecord::Schema.define do
   #########################################
   create_table :teams, force: true do |t|
     t.string 'name', null: false
-    t.string 'state'
-    t.string 'avatar_url'
+    t.string 'state', null: false
+    t.string 'avatar_url', null: false
 
     t.timestamps
   end
@@ -186,7 +186,7 @@ ActiveRecord::Schema.define do
   create_table :clubs, force: true do |t|
     t.string 'club_name', null: false
     t.string 'state'
-    t.string 'club_flag_url'
+    t.string 'club_flag_url', null: false
 
     t.timestamps
   end
